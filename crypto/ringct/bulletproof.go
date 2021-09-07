8fd49b8bc95c10c3993b55aa5adb2b89548f6041
Yang Hau <vulxj0j8j8@gmail.com>
2021-09-07 17:35:01 +0800

    remove redundant space

--- crypto/ringct/bulletproof.go
+++ crypto/ringct/bulletproof.go
@@ -22,7 +22,7 @@ import "encoding/binary"
 
 import "github.com/deroproject/derosuite/crypto"
 
-// see the  references such as original paper and multiple implementations
+// see the references such as original paper and multiple implementations
 // https://eprint.iacr.org/2017/1066.pdf
 // https://blog.chain.com/faster-bulletproofs-with-ristretto-avx2-29450b4490cd
 const maxN = 64
